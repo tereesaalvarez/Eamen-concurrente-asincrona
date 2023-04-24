9069fd9baeaa724f98e725da75de766177ecc6b6
tereesaalvarez <alvdeporteresa@gmail.com>
2023-04-24 16:52:19 +0200

    class cuenta bancaria

--- banco.py
+++ banco.py
@@ -1,3 +1,13 @@
 import threading
 import unittest
 
+class CuentaBancaria:
+    def __init__(self):
+        self.saldo = 100
+    
+    def ingresar_dinero(self,cantidad):
+        self.saldo += cantidad
+
+    def retirar_dinero(self,cantidad):
+        self.saldo -= cantidad
+
