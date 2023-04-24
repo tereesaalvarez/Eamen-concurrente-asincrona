88d8f3b695623e2f3896716ebe1f3cb66b346086
tereesaalvarez <alvdeporteresa@gmail.com>
2023-04-24 16:58:38 +0200

    ingresos

--- banco.py
+++ banco.py
@@ -11,3 +11,19 @@ class CuentaBancaria:
     def retirar_dinero(self,cantidad):
         self.saldo -= cantidad
 
+class TestCuentaBancaria(unittest.TestCase):
+    def setUp(self):
+        self.cuenta = CuentaBancaria()
+
+    def test_ingreso_y_retiro(self):
+        threads= []
+#ingreso
+        for i in range(40):
+            t = threading.Thread(target=self.cuenta.ingresar_dinero, args=(100,))
+            threads.append(t)
+        for i in range(20):
+            t = threading.Thread(target=self.cuenta.ingresar_dinero, args=(50,))
+            threads.append(t)
+        for i in range(60):
+            t = threading.Thread(target=self.cuenta.ingresar_dinero, args=(20,))
+            threads.append(t)
